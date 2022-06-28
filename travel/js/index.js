19784f7169493422e7c61d352394cf3fa4e1941e
Lada <bubnovichl@mail.ru>
2022-06-28 22:15:02 +0300

    init travel site

---
+++ travel/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+	$('.destinations-slider').slick({
+		/*arrows: false,
+		dots: true,
+		addaptiveHeight: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		speed: 300,
+		easing: "linear",
+		infinite: true,
+		initialSlide: 1,
+		autoplay: false,
+		draggable: true,
+		swipe: true,
+		waitForAnimate: false,*/
+	});
+});
